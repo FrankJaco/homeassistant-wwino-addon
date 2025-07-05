4b72a31d371c9da64e8edcc03b7f92959eefd158
fj <fjaco@att.net>
2025-07-05 11:08:13 -0400

    quick sanity check2

--- wine_inventory_scanner/WW-main.py
+++ wine_inventory_scanner/WW-main.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask, request, jsonify         #, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory
 import sqlite3
 import requests
 from bs4 import BeautifulSoup
@@ -1161,7 +1161,7 @@ def delete_wine():
     finally:
         conn.close()
 
-from flask import send_from_directory
+
 
 @app.route("/")
 def serve_frontend():
