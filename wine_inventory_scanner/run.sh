5d5e3204b6979e1811c56f16606eddaf98e35cf7
fj <fjaco@att.net>
2025-07-06 13:05:40 -0400

    Fix DB ReInit final?

--- wine_inventory_scanner/run.sh
+++ wine_inventory_scanner/run.sh
@@ -7,6 +7,7 @@ export HOME_ASSISTANT_URL=$(bashio::config 'HOME_ASSISTANT_URL')
 export HA_LONG_LIVED_TOKEN=$(bashio::config 'HA_LONG_LIVED_TOKEN')
 export TODO_LIST_ENTITY_ID=$(bashio::config 'TODO_LIST_ENTITY_ID')
 export LOG_LEVEL=$(bashio::config 'LOG_LEVEL')
+export REINITIALIZE_DATABASE=$(bashio::config 'REINITIALIZE_DATABASE')
 
 exec python3 /app/WW-main.py
 
