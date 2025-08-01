0ce40729cc04e132c617077ab6f5ed0ed79284dc
Frank Jaco <fjaco@att.net>
2025-08-01 09:22:02 -0400

    Initial public release

---
+++ wonderful_wino/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/with-contenv bashio
+
+echo "Starting Wine Inventory Scanner Addon..."
+
+# Export config values as environment variables for the Python app
+export HOME_ASSISTANT_URL=$(bashio::config 'HOME_ASSISTANT_URL')
+export HA_LONG_LIVED_TOKEN=$(bashio::config 'HA_LONG_LIVED_TOKEN')
+export TODO_LIST_ENTITY_ID=$(bashio::config 'TODO_LIST_ENTITY_ID')
+export LOG_LEVEL=$(bashio::config 'LOG_LEVEL')
+export REINITIALIZE_DATABASE=$(bashio::config 'REINITIALIZE_DATABASE')
+
+exec python3 /app/WW-main.py
+
